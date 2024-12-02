faebb225d160fa7c3a536fb2827ed2c75ab75992
André Flores <oaflores7@utpl.edu.ec>
2024-12-02 01:22:33 -0500

    Login y Registro funcionales

--- sistema_inventarios/usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.iniciar_sesion, name='login'),
-    path('registro/', views.registro, name='registro'),
-    path('home/', views.home, name='home'),
-
-]
